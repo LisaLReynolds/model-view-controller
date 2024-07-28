497130230d10e8df8b341b2c52b630b40329d622
Bowser <lisalreynolds2015@gmail.com>
2024-07-28 12:49:49 -0600

    added css files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post, Comment, User } = require("../models/");
+const { Post, Comment, User } = require("../models");
 const { withGuard, withoutGuard } = require("../utils/authGuard");
 
 router.get("/", async (req, res) => {
